573cadb9e4759cbce00979d034d99a796af7b77a
boyan <boyan.iliev@creo-tech.co>
2021-12-01 10:36:54 +0200

    Added: localization libarary and translated main page

--- untitled1/lib/features/users/presentation/users_page.dart
+++ untitled1/lib/features/users/presentation/users_page.dart
@@ -1,8 +1,10 @@
+import 'package:easy_localization/src/public_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:untitled1/features/users/domain/model/user.dart';
 import 'package:untitled1/features/users/presentation/user_presenter.dart';
 import 'package:untitled1/features/users/presentation/user_viewmodel.dart';
+import 'package:untitled1/generated/locale_keys.g.dart';
 import 'package:untitled1/list_item.dart';
 
 class UsersPage extends StatefulWidget {
@@ -34,7 +36,7 @@ class _HomeState extends State<UsersPage> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text("Lists items"),
+        title: const Text(LocaleKeys.user_list).tr(),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
@@ -48,18 +50,25 @@ class _HomeState extends State<UsersPage> {
                 return const Center(child: CircularProgressIndicator());
               }
               if (state is UserViewModelError) {
-                return const Center(child: Text("Error loading users... Please try again"));
+                return Center(
+                    child: const Text(LocaleKeys.user_list_error).tr());
               }
               if (state is UserViewModelContent) {
                 final List<User> users = state.users;
-                return ListView.builder(
-                    itemCount: users.length,
-                    itemBuilder: (context, index) {
-                      return ListItem(user: users[index], delete: () {});
-                    });
+                if (users.isEmpty) {
+                  return Center(
+                      child: const Text(LocaleKeys.user_list_no_users).tr());
+                } else {
+                  return ListView.builder(
+                      itemCount: users.length,
+                      itemBuilder: (context, index) {
+                        return ListItem(user: users[index], delete: () {});
+                      });
+                }
               }
             }
-            return const Center(child: Text("No users yet"));
+            return Center(
+                child: const Text(LocaleKeys.user_list_no_users).tr());
           },
         ),
       ),
--- untitled1/lib/main.dart
+++ untitled1/lib/main.dart
@@ -1,21 +1,39 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/features/users/domain/usecase/get_users_usecase.dart';
 import 'package:untitled1/features/users/presentation/user_presenter.dart';
 import 'package:untitled1/features/users/presentation/users_page.dart';
 import 'di/injection_container.dart';
+import 'generated/codegen_loader.g.dart';
 import 'get_data.dart';
 import 'list_item.dart';
 import 'items.dart';
 import 'package:untitled1/items_info.dart';
 import 'package:untitled1/di/injection_container.dart' as di;
 
-void main() {
+Future<void> main() async {
   di.init();
-  runApp(MaterialApp(
-    initialRoute: '/home',
-    routes: {
-      '/info': (context) => ItemsInfo(),
-      '/home': (context) => UsersPage(getIt()),
-    },
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(EasyLocalization(
+    supportedLocales: const [Locale('en'), Locale('de')],
+    path: 'assets/translations',
+    fallbackLocale: const Locale('en'),
+    assetLoader: const CodegenLoader(),
+    startLocale: const Locale('en'),
+    child: Builder(builder: (context) {
+      return MaterialApp(
+        initialRoute: '/home',
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        routes: {
+          '/info': (context) => const ItemsInfo(),
+          '/home': (context) => UsersPage(getIt()),
+        },
+      );
+    }),
   ));
 }
